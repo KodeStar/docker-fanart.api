b4ae41e4e3caa4168dd91fd7d3a9f5b532143f4b
sparklyballs <sparklyballs@gmail.com>
2015-09-17 13:48:46 +0100

    change git pull to subfolder of www

--- init/20_fetch_api.sh
+++ init/20_fetch_api.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-if [ ! -d "/config/www/system" ]; then
-rm -r /config/www
-/sbin/setuser abc git clone https://github.com/KodeStar/pro-api.git /config/www
+if [ ! -d "/config/www/fanartapi" ]; then
+/sbin/setuser abc git clone https://github.com/KodeStar/pro-api.git /config/www/fanartapi
 fi
